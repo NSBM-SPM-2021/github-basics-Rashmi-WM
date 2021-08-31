f48a5fcbf7ef1bb7d837280bd606af037d612a22
Rashmi-WM <rashmiweerasekara@gmail.com>
2021-09-01 00:39:28 +0530

    These are my new Changes
    I create Full Name, Mobile, E-mail, Address field and Save Button.

---
+++ contact-app/src/Components/ContactForm.js
@@ -0,0 +1,74 @@
+import React, {useState,useEffect} from "react";
+
+const ContactForm = () =>{
+
+    const initialFieldvalues = {
+    fullName:'',
+    mobile: '',
+    email: '',
+    address: '',
+    }
+
+    var {values,setValues} = useState(initialFieldvalues)
+
+const handleInputChange = e => {
+    var {name,value}= e.target
+    setValues({
+        ...values,
+        [name]:value
+    })
+}
+
+    return(
+        <form autoComplete="off">
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className ="fas fa-user"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="Full Name" name="fullName"
+                value={values.fullName} 
+                onChange={handleInputChange}></input>
+            
+            </div>
+        
+            <div className="form-row">
+            <div className="form-group input-group col-md-6">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className ="fas fa-mobile-alt"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="Mobile" name="mobile"
+                value={values.mobile}
+                onChange={handleInputChange}></input>
+            
+            </div>
+            <div className="form-group input-group col-md-6">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className ="fas fa-envelope"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="E-mail" name="email"
+                value={values.email}
+                 onChange={handleInputChange}></input>
+            
+            </div>  
+            <div className="form-group">
+                <textarea className="form-control" placeholder="Address" name="address"
+                value={values.address}
+                onChange={handleInputChange}></textarea>
+            </div>
+            <div className="form-group">
+                <input type="submit" value="Save" 
+                className="btn btn-primary btn-block"/>
+            </div>
+            </div>
+            
+        </form>
+
+    );
+}
+export default ContactForm;
