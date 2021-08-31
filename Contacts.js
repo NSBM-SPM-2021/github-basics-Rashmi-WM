e9c2c9932b7b87be392dca07c83e8a792e6410fa
Rashmi Ishara <69496265+Rashmi-WM@users.noreply.github.com>
2021-08-31 22:03:16 +0530

    This is my new change -No1 , text-center

--- Contacts.js
+++ Contacts.js
@@ -7,7 +7,7 @@ const Contacts = () =>{
         <React.Fragment>
         <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4 ">Contact Register</h1>
+    <h1 class="display-4 text-center ">Contact Register</h1>
     
   </div>
 </div>
