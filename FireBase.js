c04727b6855b119b8e533176f6b52efe1b61e2c3
Rashmi Ishara <69496265+Rashmi-WM@users.noreply.github.com>
2021-10-13 00:22:40 +0530

    Update firebase.js

--- FireBase.js
+++ FireBase.js
@@ -3,12 +3,13 @@ import 'firebase/compat/firestore';
 
 
 var firebaseApp = firebase.initializeApp ({
-    apiKey: "AIzaSyBwYqnRTlLqthR5wIgQkrNHjwprTOpTKpM",
-    authDomain: "react-contact-app-28f96.firebaseapp.com",
-    projectId: "react-contact-app-28f96",
-    storageBucket: "react-contact-app-28f96.appspot.com",
-    messagingSenderId: "104524223909",
-    appId: "1:104524223909:web:145bee688c6c871e8b444c"
+    apiKey: "AIzaSyAKQlXxoNp_6lAJaR4of1vCal2XELrTZQU",
+  authDomain: "spm2021-64cee.firebaseapp.com",
+  databaseURL: "https://spm2021-64cee-default-rtdb.firebaseio.com",
+  projectId: "spm2021-64cee",
+  storageBucket: "spm2021-64cee.appspot.com",
+  messagingSenderId: "121172586358",
+  appId: "1:121172586358:web:58f160693e5710a8609457"
 });
 
 var db = firebaseApp.firestore();
